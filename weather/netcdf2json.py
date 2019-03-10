0bae85e9acd8f17000c6c563e35ac2c8547e22b2
huanglii <849151701@qq.com>
2019-03-10 14:46:07 +0800

    nc2json

--- weather/netcdf2json.py
+++ weather/netcdf2json.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-from netCDF4 import Dataset, num2date
+from netCDF4 import Dataset
 
 np.set_printoptions(precision=1)
 
@@ -9,20 +9,27 @@ ds = Dataset(nc_file, mode='r')
 
 nx = ds.dimensions['longitude'].size
 ny = ds.dimensions['latitude'].size
-# print(num2date(2.019031e+13, 'seconds'))
 
 lons = ds.variables['longitude'][:]
 lats = ds.variables['latitude'][:]
 
+def vacuate(data):
+    d_1 = data[::20]
+    for i in range(len(d_1)):
+        d_1[i] = d_1[i][::20]
+    return np.array(d_1)
+
 u_grid = ds.variables['UGRD_10maboveground'][:]
-u_data = np.around(u_grid.flatten(), decimals=1)
+u_grid_vac = vacuate(u_grid.tolist())
+u_data = np.around(u_grid_vac.flatten(), decimals=1).tolist()
+
+
 v_grid = ds.variables['VGRD_10maboveground'][:]
-v_data = np.around(v_grid.flatten(), decimals=1)
+v_grid_vac = vacuate(v_grid.tolist())
+v_data = np.around(v_grid_vac.flatten(), decimals=1).tolist()
 
-u_data[u_data == 9999.0] = 0
-v_data[v_data == 9999.0] = 0
-
-print(len(u_grid[0]))
+u_data[u_data == 9999.0] = None
+v_data[v_data == 9999.0] = None
 
 
 header = {
@@ -31,33 +38,31 @@ header = {
     'parameterNumber': 2,
     'parameterNumberName': 'U-component_of_wind',
     'parameterUnit': 'm.s-1',
-    'numberPoints': 1682601,
+    'numberPoints': 4331,
     'gridUnits': 'degrees',
     'resolution': 48,
-    'nx': 1401,
-    'ny': 1201,
+    'nx': 71,
+    'ny': 61,
     'lo1': 70,
     'lo2': 140,
     'la1': 60,
     'la2': 0,
-    'dx': 0.05,
-    'dy': 0.05
+    'dx': 1,
+    'dy': 1
 }
 u = {
     'header': header.copy(),
-    'data': u_data.tolist()
+    'data': u_data
 }
 v = {
     'header': header.copy(),
-    'data': v_data.tolist()
+    'data': v_data
 }
 v['header']['parameterNumberName'] = 'V_component_of_current'
 v['header']['parameterNumber'] = 3
 
-js = {'s': 'you', 'd': 'are', 'd': {'d': 1}}
 
-
-f = open('gfs1.0.json', 'w')
+f = open('current-wind-surface-level-gfs-1.0.json', 'w')
 f.write('[')
 f.write(json.dumps(u))
 f.write(',')
@@ -65,5 +70,6 @@ f.write(json.dumps(v))
 f.write(']')
 
 f.close()
+ds.close()
 
 
--- weather/netcdf_viz.py
+++ weather/netcdf_viz.py
@@ -7,8 +7,16 @@ ds = Dataset(nc_file, mode='r')
 
 tmax = ds.variables['TMAX_2maboveground'][:]
 data = np.mat(tmax[::-1])
-plt.xticks([]), plt.yticks([])
-plt.imshow(data)
-plt.colorbar(shrink=.92)
+
+plt.figure(figsize=(14, 12), dpi=100)
+plt.gca().xaxis.set_major_locator(plt.NullLocator())
+plt.gca().yaxis.set_major_locator(plt.NullLocator())
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+plt.margins(0, 0)
+
+#plt.xticks([]), plt.yticks([])
+plt.imshow(data, aspect='equal')
+# plt.colorbar(shrink=.92)
+plt.savefig('2019030600_003.png', format='png', transparent=True, pad_inches=0)
 plt.show()
 ds.close()
